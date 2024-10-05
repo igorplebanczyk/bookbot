fe04196d2b6d89cfd779a4ba944f1fd60cc64326
igorplebanczyk <plebanczyk.igor@gmail.com>
2024-10-05 23:06:04 +0200

    feat: add word frequency counting

--- internal/text_analyzer.py
+++ internal/text_analyzer.py
@@ -1,23 +1,43 @@
 class TextAnalyzer:
     def __init__(self, text):
-        self.text = text
-        self.word_count = self.get_num_words()
-        self.letter_count = self.get_letters_count()
+        self.text: str = text
+        self.word_count: int = self.get_num_words()
+        self.letter_count: list = self.get_letters_count()
+        self.common_words: list = self.get_most_common_words(25)
 
-    def get_num_words(self):
+    def get_num_words(self) -> int:
         return len(self.text.split())
 
-    def get_letters_count(self):
-        letter_count = {}
+    def get_letters_count(self) -> list:
+        letter_count_dict = {}
         for letter in self.text.lower():
             if letter.isalpha():
-                letter_count[letter] = letter_count.get(letter, 0) + 1
-        return letter_count
+                letter_count_dict[letter] = letter_count_dict.get(letter, 0) + 1
 
-    def format_report(self):
+        return sorted(letter_count_dict.items(), key=lambda item: item[1], reverse=True)
+
+    def get_most_common_words(self, n: int) -> list:
+        words = self.text.split()
+        word_count = {}
+
+        for word in words:
+            word_count[word] = word_count.get(word, 0) + 1
+
+        return sorted(word_count.items(), key=lambda item: item[1], reverse=True)[:n]
+
+    def format_report(self) -> str:
         report = "--- Begin report ---\n"
         report += f"{self.word_count} words found in the document\n\n"
-        for letter, count in sorted(self.letter_count.items(), key=lambda item: item[1], reverse=True):
-            report += f"The '{letter}' character was found {count} times\n"
+
+        report += "Letter frequency:\n"
+        for letter, count in self.letter_count:
+            report += f"'{letter}' : {count}\n"
+        report += "\n"
+
+        report += "Word frequency:\n"
+        for word, count in self.common_words:
+            report += f"'{word}' : {count}\n"
+        report += "\n"
+
         report += "--- End report ---\n"
         return report
